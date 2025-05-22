ded64726aef3e18890599391e4b75e93bf0d6344
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2025-05-21 22:59:34 -0500

    Initial commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+type ButtonProps = {
+    onclick: () => void;
+    label: string;
+}
+
+const Button = ({onclick, label} : ButtonProps) => {
+
+    return (
+        <button onClick={onclick}>{label}</button>
+    )
+}
+
+export default Button;
